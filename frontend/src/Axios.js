78fd0cb60db80abc2c8246533e1d9effa78c262d
m-arslaan1 <mohd.arslaan49@gmail.com>
2024-12-25 19:10:45 +0530

    update

--- frontend/src/Axios.js
+++ frontend/src/Axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'https://task-manager-task-28.onrender.com';
+const API_URL = 'https://task-manager-task-28.onrender.com/api/tasks';
 
 export const getTasks = async () => {
   const response = await axios.get(`${API_URL}`);
