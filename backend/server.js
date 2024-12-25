68ed0dc128157cfff14ac77c22b2c3e4c0654e70
m-arslaan1 <mohd.arslaan49@gmail.com>
2024-12-25 13:05:13 +0530

    updated

--- backend/server.js
+++ backend/server.js
@@ -10,24 +10,31 @@ const port = process.env.PORT || 4500;
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.json());
 
-const allowedOrigins = process.env.ALLOWED_ORIGINS ? process.env.ALLOWED_ORIGINS.split(',') : ['http://localhost:5173', 'https://task-manager-0001.netlify.app'];
+const allowedOrigins = process.env.ALLOWED_ORIGINS ? process.env.ALLOWED_ORIGINS.split(',') : ['http://localhost:5173', 'https://tasks-manager-001.netlify.app'];
 
 app.use(cors({
-  origin: allowedOrigins,
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], 
   allowedHeaders: ['Content-Type', 'Authorization'],
-})); 
+}));
+
 app.use("/api", taskRoutes);
 
 mongoose
-.connect(process.env.DB_URI)
-.then(() => {
+  .connect(process.env.DB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
     console.log("Successfully connected to MongoDB");
-})
-.catch((err) => {
+  })
+  .catch((err) => {
     console.log("Error", err);
-});
+  });
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+  console.log(`Server is running on port ${port}`);
 });
