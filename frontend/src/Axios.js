2ddf2514af536cb9791376b92dbb6da7727fae17
m-arslaan1 <mohd.arslaan49@gmail.com>
2024-12-26 11:54:54 +0530

    update

--- frontend/src/Axios.js
+++ frontend/src/Axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:3000/api/tasks';
+const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:4500/api/tasks';
 
 export const getTasks = async () => {
   const response = await axios.get(`${API_URL}`);
